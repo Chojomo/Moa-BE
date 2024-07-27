90ee501f8d1487a9432db5188c00e9eca6d3a8a0
ichubtou <ichubtou@naver.com>
2024-07-28 02:52:25 +0900

    feat: Security 인증 로직 Security Configuration 적용 #31

---
+++ src/main/java/com/moa/global/security/handler/UserAuthenticationSuccessHandler.java
@@ -0,0 +1,15 @@
+package com.moa.global.security.handler;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+public class UserAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+
+    }
+
+}
