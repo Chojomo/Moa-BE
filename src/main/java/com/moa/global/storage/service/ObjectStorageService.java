876f500f814c8cda8a7c3ad966afdcc3ae0085af
ichubtou <ichubtou@naver.com>
2024-09-02 23:15:48 +0900

    feat: ObjectStorage 이미지 저장 기능 구현 #51

---
+++ src/main/java/com/moa/global/storage/service/ObjectStorageService.java
@@ -0,0 +1,47 @@
+package com.moa.global.storage.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetUrlRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+import java.io.IOException;
+import java.util.Collections;
+
+@Service
+@RequiredArgsConstructor
+public class ObjectStorageService {
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    private final S3Client s3Client;
+
+    public String uploadFile(MultipartFile multipartFile) throws IOException {
+        String fileName = multipartFile.getOriginalFilename();
+
+        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                .bucket(bucket)
+                .contentType(multipartFile.getContentType())
+                .contentLength(multipartFile.getSize())
+                .key(fileName)
+                .metadata(Collections.singletonMap("Content-Disposition", "inline"))
+                .build();
+
+        RequestBody requestBody = RequestBody.fromBytes(multipartFile.getBytes());
+
+        s3Client.putObject(putObjectRequest, requestBody);
+
+        GetUrlRequest getUrlRequest = GetUrlRequest.builder()
+                .bucket(bucket)
+                .key(fileName)
+                .build();
+
+        return String.valueOf(s3Client.utilities().getUrl(getUrlRequest));
+    }
+
+}
