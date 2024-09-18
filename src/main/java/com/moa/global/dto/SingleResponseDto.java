143a4a85ed78c88ad710c34f881fc0df3cf8f0db
ichubtou <ichubtou@naver.com>
2024-09-18 17:44:30 +0900

    fix: #92 충돌 오류 수정 #94

--- src/main/java/com/moa/global/dto/SingleResponseDto.java
+++ src/main/java/com/moa/global/dto/SingleResponseDto.java
@@ -3,12 +3,18 @@ package com.moa.global.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Builder
 @Getter
 @AllArgsConstructor
 public class SingleResponseDto<T> {
 
+    public SingleResponseDto(Integer status) {
+        this.status = status;
+    }
+
+    Integer status;
     T data;
 
 }
